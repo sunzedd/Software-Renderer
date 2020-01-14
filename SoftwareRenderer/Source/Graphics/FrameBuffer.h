506c561853afa3a0ac498a8890657b9ea5e7c8e0
sunzedd <sunz89@yandex.ru>
2020-01-14 19:12:03 +0300

    Added 2 methods to set pixel with vec3 and vec4 normalized values.

--- SoftwareRenderer/Source/Graphics/FrameBuffer.h
+++ SoftwareRenderer/Source/Graphics/FrameBuffer.h
@@ -7,11 +7,21 @@ namespace Core
 	class FrameBuffer : public Buffer<Color>
 	{
 	public:
-		FrameBuffer(int width, int height)
+		FrameBuffer( int width, int height )
 			:
-			Buffer(width, height)
+			Buffer( width, height )
 		{ }
+
+		void setPixel( int x, int y, const Vec4& color )
+		{
+			Buffer::set( x, y, Color( color ) );
+		}
 		
+		void setPixel(int x, int y, const Vec3& color)
+		{
+			Buffer::set( x, y, Color( color ) );
+		}
+
 		const Color* pixels() const
 		{
 			return m_pData.get();
