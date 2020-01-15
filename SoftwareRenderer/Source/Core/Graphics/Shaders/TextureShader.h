e76f698a7a3297c5d1115214a66d2b393c8f8fd8
sunzedd <sunz89@yandex.ru>
2020-01-15 18:49:27 +0300

    Fixed texture clamping in texture shader.

--- SoftwareRenderer/Source/Core/Graphics/Shaders/TextureShader.h
+++ SoftwareRenderer/Source/Core/Graphics/Shaders/TextureShader.h
@@ -11,8 +11,14 @@ namespace Core
 	public:
 		Vec4 pixelShader( const VSO& interpolated ) override
 		{
-			int x = m_pTexture->getSize().x * interpolated.uv.x;
-			int y = m_pTexture->getSize().y * interpolated.uv.y;
+			// clamp texture
+			float uv_x = std::max(0.0f, interpolated.uv.x);
+			float uv_y = std::max(0.0f, interpolated.uv.y);
+			uv_x = std::min(uv_x, 1.0f);
+			uv_y = std::min(uv_y, 1.0f);
+
+			int x = abs(( m_pTexture->getSize().x * uv_x - 1 ));
+			int y = abs(( m_pTexture->getSize().y * uv_y - 1 ));
 
 			sf::Color texel = m_pTexture->getPixel( x, y );
 			Color c( texel.r, texel.g, texel.b, texel.a );
