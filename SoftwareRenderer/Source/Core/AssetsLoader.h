d2469ea4ea4cf9d36572067b53d4006fa44f3e32
sunzedd <sunz89@yandex.ru>
2020-02-10 22:09:44 +0300

    Added class for loading resources.

---
+++ SoftwareRenderer/Source/Core/AssetsLoader.h
@@ -0,0 +1,23 @@
+#pragma once
+#include "Graphics/Mesh.h"
+#include <iostream>
+#include "App.h"
+
+#define _CRT_SECURE_NO_WARNINGS
+
+namespace Core
+{
+	class AssetsLoader
+	{
+	public:
+		class Exception : public App::Exception
+		{
+		public:
+			Exception(const std::string& msg)
+				:
+				App::Exception(msg) { }
+		};
+
+		static std::shared_ptr<Mesh> loadMesh(const std::string& filepath);
+	};
+}
