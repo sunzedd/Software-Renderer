80fea0ba8bca697470c1ed742e9105f60749ba48
sunzedd <sunz89@yandex.ru>
2020-02-24 23:39:27 +0300

    Updated main and vs files.;

--- SoftwareRenderer/Source/Main.cpp
+++ SoftwareRenderer/Source/Main.cpp
@@ -1,19 +1,19 @@
 #include <iostream>
 #include "Sandbox/DemoApp_Camera.h"
-#include "Sandbox/Sb1/DemoApp_New.h"
+#include "Sandbox/Test1/TestDemo1.h"
 
 int main()
 {
 	try
 	{
-		DemoApp_Camera a;
-		a.run();
+		auto app = Demo::TestApp1::Create();
+		app->run();
 	}
 	catch ( const std::exception & e )
 	{
 		std::cout << e.what() << std::endl;
 	}
 	
-	
+	//system("pause");
 	return 0;
 }
