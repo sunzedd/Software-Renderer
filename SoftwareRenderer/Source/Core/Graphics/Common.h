180293086555be6965369860a393154ed00b8d58
sunzedd <sunz89@yandex.ru>
2020-02-26 19:40:34 +0300

    Added typedef and aliases for Line.

---
+++ SoftwareRenderer/Source/Core/Graphics/Common.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <vector>
+
+namespace Core 
+{
+	class Vec3;
+	class Vec2i;
+	class Vertex;
+
+	template <class T>
+	using Line = std::pair<T, T>;
+
+	typedef Line<Vec2i>		LineV2i;
+	typedef Line<Vec3>		LineV3;
+	typedef Line<Vertex>	LineVtx;
+}
