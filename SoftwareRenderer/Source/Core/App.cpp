a07eb770a6975e5fbc1fcf857592d1cdf87f3120
Rostislav Vivcharuk <sunz89@yandex.ru>
2020-03-17 22:01:28 +0300

    Codestyle.

--- SoftwareRenderer/Source/Core/App.cpp
+++ SoftwareRenderer/Source/Core/App.cpp
@@ -1,32 +1,31 @@
 #include "App.h"
 
-namespace Core
+namespace Core {
+App::App()
+	:
+	m_window(),
+	m_windowEvent(m_window.getEvent()),
+	m_renderer(m_window.getFrameBuffer())
+{}
+
+App::App(int width, int height, const std::string& title, bool fullscreen)
+	:
+	m_window(width, height, title, fullscreen),
+	m_windowEvent(m_window.getEvent()),
+	m_renderer(m_window.getFrameBuffer())
+{}
+
+App::~App()
+{}
+
+void App::run()
 {
-	App::App()
-		:
-		m_window(),
-		m_windowEvent(m_window.getEvent()),
-		m_renderer(m_window.getFrameBuffer())
-	{}
-
-	App::App(int width, int height, const std::string& title, bool fullscreen)
-		:
-		m_window(width, height, title, fullscreen),
-		m_windowEvent(m_window.getEvent()),
-		m_renderer(m_window.getFrameBuffer())
-	{}
-
-	App::~App()
-	{}
-
-	void App::run()
+	while (m_window.isOpen())
 	{
-		while (m_window.isOpen())
-		{
-			sf::Time dtime = m_timer.restart();
-			update(dtime.asMilliseconds());
-			render(dtime.asMilliseconds());
-			m_window.update(dtime);
-		}
+		sf::Time dtime = m_timer.restart();
+		update(dtime.asMilliseconds());
+		render(dtime.asMilliseconds());
+		m_window.update(dtime);
 	}
 }
+} // namespace Core
