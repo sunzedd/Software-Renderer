71009a42d685c65e265af775e645b9e24a3a02c9
sunzedd <sunz89@yandex.ru>
2020-03-31 16:44:03 +0300

    Added getters for mesh and texture.

--- SoftwareRenderer/Source/Core/Scene/Object3D.cpp
+++ SoftwareRenderer/Source/Core/Scene/Object3D.cpp
@@ -27,6 +27,16 @@ std::shared_ptr<ShaderProgram> Object3D::getShader()
     return m_shader;
 }
 
+const std::shared_ptr<Mesh>& Object3D::getMesh() const
+{
+    return m_mesh;
+}
+
+const std::shared_ptr<sf::Image>& Object3D::getTexture() const
+{
+    return m_texture;
+}
+
 void Object3D::setShader(std::shared_ptr<ShaderProgram> shader) 
 {
     if (shader)
@@ -37,6 +47,12 @@ void Object3D::setMesh(std::shared_ptr<Mesh> mesh)
 {
     if (mesh)
         m_mesh = mesh;
+}
+
+void Object3D::setTexture(std::shared_ptr<sf::Image> texture)
+{
+    if (texture)
+        m_texture = texture;
 }
 
 } // namespace core
--- SoftwareRenderer/Source/Core/Scene/Object3D.h
+++ SoftwareRenderer/Source/Core/Scene/Object3D.h
@@ -13,13 +13,17 @@ public:
     virtual void update(unsigned int deltaTime) override;
 
     std::shared_ptr<ShaderProgram> getShader();
+    const std::shared_ptr<Mesh>& getMesh() const;
+    const std::shared_ptr<sf::Image>& getTexture() const;
 
     void setShader(std::shared_ptr<ShaderProgram> shader);
     void setMesh(std::shared_ptr<Mesh> mesh);
+    void setTexture(std::shared_ptr<sf::Image> texture);
 
 protected:
     std::shared_ptr<ShaderProgram> m_shader;
     std::shared_ptr<Mesh> m_mesh;
+    std::shared_ptr<sf::Image> m_texture;
 }; 
 
 } // namespace core
