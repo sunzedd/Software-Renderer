e2932c49398cbf64b4170bd311200e867d3f6640
sunzedd <sunz89@yandex.ru>
2020-04-01 20:37:51 +0300

    Created phong shader.

---
+++ SoftwareRenderer/Source/Sandbox/Shaders/Aliases.h
@@ -0,0 +1,9 @@
+#pragma once
+#include "../../Core/Graphics/ShaderProgram.h"
+
+using v3 = core::Vec3;
+using v4 = core::Vec4;
+using v2 = core::Vec2;
+using v2i = core::Vec2i;
+using vs_output = core::VSO;
+using vertex = core::Vertex;
